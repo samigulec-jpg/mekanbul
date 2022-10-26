7e1c54eaace450946d3a7aa4bfeebba14b2bf37a
samigulec-jpg <bekir-sami02@hotmaiil.com>
2022-10-26 17:32:02 +0300

    ödev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,13 +2,84 @@ var express= require('express');
 var router = express.Router();
 
 const anaSayfa=function(req,res,next){
-    res.render('anasayfa', { title: 'Ana Sayfa' });
+    res.render('anasayfa', 
+    { "baslik": 'Ana Sayfa',
+    "sayfaBaslik":{
+        "siteAd":"MekanBul",
+        "slogan":"Civardaki Mekanları Keşfet!"
+    },
+    "mekanlar":[
+        {
+            "ad":"Barida Kafe",
+            "adres":"Sdü Batı Kampüsü",
+            "puan":"4",
+            "mesafe":"2km",
+            "imkanlar":["Kahve","Çay","Pasta"]
+        },
+        {
+            "ad":"Gloria Jeans",
+            "adres":"Sdü Doğu Kampüsü",
+            "puan":"4",
+            "mesafe":"5km",
+            "imkanlar":["Kahve","Çay","Pasta"]
+            }    
+
+
+    ]
+
+
+});
 
 
 }
 
 const mekanBilgisi=function(req,res,next){
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
+    res.render('mekanbilgisi', 
+    { "baslik": 'Mekan Bilgisi',
+    "mekanBaslik":"Starbucks",
+    "mekanDetay":{
+        "ad":"Starbucks",
+        "adres":"Centrum Garden",
+        "puan":"4",
+        "saatler":[
+            {
+                "gunler":"Pazartesi-Cuma",
+                "acilis":"9.00",
+                "kapanis":"23.00",
+                "kapali": false
+            },
+            {
+                "gunler":"Cumartesi-Pazar",
+                "acilis":"10.00",
+                "kapanis":"22.00",
+                "kapali": false
+            },
+        ],
+        "imkanlar":["kahve","çay","kek"],
+        "koordinatlar":{
+            "enlem":"37.7",
+            "boylam":"30.5"
+        },
+        "yorumlar":[
+            {
+                "yorumYapan":"Sami",
+                "puan":"2",
+                "tarih":"14 Ekim 2022",
+                "yorumMetni":"Berbat.."
+            },
+            {
+                "yorumYapan":"Barış",
+                "puan":"5",
+                "tarih":"19 Ekim 2022",
+                "yorumMetni":"İyiydi.."
+            }
+        ]
+
+
+    } 
+
+
+});
 
 
 }
