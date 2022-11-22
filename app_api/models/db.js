b07eeeae371b525175b4d481683b88337bb95e87
samigulec-jpg <bekir-sami02@hotmaiil.com>
2022-11-22 21:37:04 +0300

    odev5

---
+++ app_api/models/db.js
@@ -0,0 +1,27 @@
+var mongoose=require('mongoose');
+//var dbURI="mongodb://localhost/mekanbul";
+var dbURI="mongodb+srv://sami:mekan123@mekanbul.cgbejr4.mongodb.net/mekanbul?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback();
+    });
+
+}
+process.on("SIGINT",function(){
+    kapat("Uygulama kapatıldı",function(){
+        process.exit(0)
+    });
+});
+mongoose.connection.on("connected", function(){
+    console.log(dbURI+" adresindeki veritabanına bağlandı.");
+});
+mongoose.connection.on("disconnected", function(){
+    console.log(dbURI+" Bağlantı koptu.");
+});
+mongoose.connection.on("error", function(){
+    console.log(dbURI+"BAĞLANTI HATASI");
+});
+
+require("./mekansema");
